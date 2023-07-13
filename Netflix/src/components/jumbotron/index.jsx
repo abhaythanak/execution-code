112917686f712297d1ff1dea471801c104711542
Abhay Thanak <abhaythanak@gmail.com>
2023-07-13 23:08:38 +0530

    footer component building

--- Netflix/src/components/jumbotron/index.jsx
+++ Netflix/src/components/jumbotron/index.jsx
@@ -1,5 +1,7 @@
 import { Container, Inner, Pane, Title, SubTitle, Item, Image } from "./styles/jumbotron"
 
+
+
 export default function Jumbotron({ children, direction = 'row', ...restProps}) {
     return(
     <Item  {...restProps}>
